9727ffb5efd2481ce5301bae83155e696a856c39
kenokento <kento26615@g.lfis.edu.hk>
2025-02-12 10:11:28 +0800

    Update main.py3

--- main.py
+++ main.py
@@ -18,7 +18,16 @@ id_voiture_1 = Voiture('Ford Mustang', 'RWD', 450, 10, 60, 'Petrol', 2022, 'Ford
 id_voiture_2 = Voiture('Honda CR V', 'AWD', 190, 12, 57, 'Petrol', 2025, 'Honda', 'SUV', 5, 700, 80000, 100000)
 id_voiture_3 = Voiture('BMW X5', 'RWD', 335, 11, 83, 'Petrol', 2008, 'BMW', 'SUV', 5, 650, 100000, 180000)
 id_voiture_4 = Voiture('Toyota Camry', 'RWD', 203, 15, 60, 'Petrol', 2001, 'Toyota', 'Sedan', 5, 500, 40000, 80000)
+id_voiture_5 = Voiture('Chevrolet Camaro', 'RWD', 455, 9, 63, 'Petrol', 2023, 'Chevrolet', 'Coupe', 4, 250, 75000, 130000)
+id_voiture_6 = Voiture('Nissan Rogue', 'AWD', 170, 26, 54, 'Petrol', 2024, 'Nissan', 'SUV', 5, 800, 32000, 42000)
+id_voiture_7 = Voiture('Volkswagen Passat', 'FWD', 174, 29, 62, 'Petrol', 2020, 'Volkswagen', 'Sedan', 5, 400, 28000, 36000)
 
+#AFFICHER
+
+def afficher_voitures():
+    cursor.execute("SELECT voiture FROM voitures")
+    voiture = cursor.fetchall()
+    print(voitures)
 
 #INSERT
 
@@ -40,7 +49,6 @@ def insert_voiture(voiture):
     'prix_nouveau': voiture.prix_nouveau
 })
 
-
 #SUPPRIMER
 
 def rm_voiture(voiture):
@@ -49,15 +57,89 @@ def rm_voiture(voiture):
             'nom': voiture.nom
         })
 
-
-
 #UPDATE V
 
 def update_donne(voiture):
+    pass
 
 
+def select_voiture(voiture):
+    cursor.execute('''
+    SELECT * FROM voitures
+    WHERE nom = {attribut}) 
+    AND transmission = {attribut}) 
+    AND puissance BETWEEN {value} AND {value} 
+    AND kpl BETWEEN {value} AND {value}
+    AND
+    reservoir BETWEEN {value} AND {value}
+    AND
+    engin = {attribut}
+    AND
+    annee BETWEEN {value} AND {value}
+    AND
+    marque {attribut}
+    AND
+    type {attribut}
+    AND
+    siege {attribut}
+    AND
+    espace BETWEEN {value} AND {value}
+    AND
+    prix_utilise BETWEEN {value} AND {value}
+    AND
+    prix_nouveau BETWEEN {value} AND {value}
+    AND
+    classement = {attribut}
+    ''')
+    return cursor.fetchall
 
 
+#MAIN------------------------------------------------------------------------------------------------------------------------
+
+
+def main():
+    if lien_fichier('BDD_Voiture.db') = True
+        print("Menu:")
+        print("1 Afficher les voitures")
+        print("2 Filtrer les voitures")
+        print("3 Ajouter une voiture")
+        print("4 Mettre à jour une voiture")
+        print("5 Supprimer une voiture")
+        print("6 Quitter")
+        option = input("Choisissez une option: ")
+        if option == '1':
+            afficher_voitures()
+        if option == '2':
+            filtre_voitures()
+        if option == '3':
+            add_voitures()
+        if option == '4':
+            update_voitures()
+        if option == '5':
+            delete_voitures()
+        if option == '6':
+            exit()
+        else:
+            print("Erreur")
+            
+       
+
+
+def filtre():
+nom = input("Filtrer par nom [vide pour ignorer]: ") 
+transmission = input("Filtrer par transmission [vide pour ignorer]: ")  #cm
+puissance = input("Filtrer par nom [vide pour ignorer]: ") #cm
+kpl = input("Filtrer par nom [vide pour ignorer]: ") #cm
+reservoir = input("Filtrer par nom [vide pour ignorer]: ") #cm
+engin = input("Filtrer par nom [vide pour ignorer]: ") #cm
+annee = input("Filtrer par nom [vide pour ignorer]: ") 
+marque = input("Filtrer par nom [vide pour ignorer]: ") 
+type = input("Filtrer par nom [vide pour ignorer]: ") 
+siege = input("Filtrer par nom [vide pour ignorer]: ") 
+espace = input("Filtrer par nom [vide pour ignorer]: ") 
+prix_utilise = input("Filtrer par nom [vide pour ignorer]: ") 
+prix_nouveau = input("Filtrer par nom [vide pour ignorer]: ") 
+classement = 
 
 
 
@@ -142,55 +224,3 @@ def delete_voiture(nom, transmission,	puissance,	kpl,	reservoir,	engin,	annee,	m
 
 
 
-
-#MAIN------------------------------------------------------------------------------------------------------------------------
-
-
-def main():
-    if lien_fichier('BDD_Voiture.db') = True
-        print("Menu:")
-        print("1 Afficher les voitures")
-        print("2 Filtrer les voitures")
-        print("3 Ajouter une voiture")
-        print("4 Mettre à jour une voiture")
-        print("5 Supprimer une voiture")
-        print("6 Quitter")
-        option = input("Choisissez une option: ")
-        if option == '1':
-            afficher_voitures()
-        if option == '2':
-            filtre_voitures()
-        if option == '3':
-            add_voitures()
-        if option == '4':
-            update_voitures()
-        if option == '5':
-            delete_voitures()
-        if option == '6':
-            exit()
-        else:
-            print("Erreur")
-            
-       
-
-def afficher_voitures():
-    cursor.execute("SELECT voiture FROM voitures")
-    voiture = cursor.fetchall()
-    print(voitures)
-
-
-def filtre():
-nom = input("Filtrer par nom [vide pour ignorer]: ") 
-transmission = input("Filtrer par transmission [vide pour ignorer]: ")  #cm
-puissance = input("Filtrer par nom [vide pour ignorer]: ") #cm
-kpl = input("Filtrer par nom [vide pour ignorer]: ") #cm
-reservoir = input("Filtrer par nom [vide pour ignorer]: ") #cm
-engin = input("Filtrer par nom [vide pour ignorer]: ") #cm
-annee = input("Filtrer par nom [vide pour ignorer]: ") 
-marque = input("Filtrer par nom [vide pour ignorer]: ") 
-type = input("Filtrer par nom [vide pour ignorer]: ") 
-siege = input("Filtrer par nom [vide pour ignorer]: ") 
-espace = input("Filtrer par nom [vide pour ignorer]: ") 
-prix_utilise = input("Filtrer par nom [vide pour ignorer]: ") 
-prix_nouveau = input("Filtrer par nom [vide pour ignorer]: ") 
-classement = 
